3c30baec134aed113d73a34e7a56edafe7451a68
haithmak <haitham.almaqtari@gmail.com>
2020-01-24 20:26:05 +0300

    add new echo by hathmak90 2

--- index.php
+++ index.php
@@ -4,4 +4,8 @@ echo " New News in the World " ;
 
 
 //Added by hathmak90
-echo " New News in the World " ;
+echo " New News in the World 1 " ;
+
+//Added by hathmak90 - 2
+
+echo " New News in the World 2 " ;
